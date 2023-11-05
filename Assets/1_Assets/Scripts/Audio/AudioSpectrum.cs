83d0de438563e8bc9c9cd4f356d51a27c316fcc6
kimduc1409 <kimduc14092003@gmail.com>
2023-11-05 18:30:26 +0700

    update audio sync

--- Assets/1_Assets/Scripts/Audio/AudioSpectrum.cs
+++ Assets/1_Assets/Scripts/Audio/AudioSpectrum.cs
@@ -6,6 +6,7 @@ public class AudioSpectrum : MonoBehaviour
 {
     public static float spectrumValue { get; private set; }
     private float[] m_audioSpectrum;
+    public FFTWindow m_window;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,7 @@ public class AudioSpectrum : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        AudioListener.GetSpectrumData(m_audioSpectrum, 0, FFTWindow.Hamming);
+        AudioListener.GetSpectrumData(m_audioSpectrum, 0, m_window  );
         if (m_audioSpectrum != null && m_audioSpectrum.Length > 0)
         {
             spectrumValue = m_audioSpectrum[0]*100;
